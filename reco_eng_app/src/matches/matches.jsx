7c9fa61a00e437921d892206da429f3e4182cd72
Christian Bryan <cdbryan@usc.edu>
2023-04-05 13:35:20 +0200

    matches css improved

--- reco_eng_app/src/matches/matches.jsx
+++ reco_eng_app/src/matches/matches.jsx
@@ -9,6 +9,10 @@ import Grid from "@mui/material/Grid";
 import Carousel from "react-material-ui-carousel";
 import Box from "@mui/material/Box";
 import workCafe from "../images/WorkCafe.jpeg";
+import verticalCafe from "../images/verticalImage.jpeg";
+import blob1 from "../images/blob1.png"
+import blob2 from "../images/blob2.png"
+
 
 export default function Matches() {
   const navigate = useNavigate();
@@ -26,31 +30,43 @@ export default function Matches() {
       });
   };
 
-  const [testArr, setTestArr] = React.useState([workCafe, workCafe, workCafe]);
+  const [testArr, setTestArr] = React.useState([
+    workCafe,
+    workCafe,
+    verticalCafe,
+    verticalCafe,
+    verticalCafe
+  ]);
 
   return (
     <>
       <Navbar></Navbar>
       <div id="matchesHeader">
-        <h1>Your Workspace Matches</h1>
-      </div>
+        <h1 id="yourWorkspaceMatches">Your Workspace Matches</h1>
+     
 
       <Grid
         container
         spacing={3}
-        sx={{ alignItems: "center", justifyContent: "center" }}
+        sx={{justifyContent: "flex-begin" }}
       >
         {testArr.map((item) => (
-          <Grid item xs={2} md={3}>
-            <Box sx={{border: "1px solid blue"}}>
-              <Box sx={{height: 200}}>
-                <Carousel id="carouselStyle" indicators={true} autoPlay={false} sx={{border: "1px solid orange"}} >
-                  <div id="thumbnail">
-                    <img id="thumbnailImg" src={item}></img>
-                  </div>
-                </Carousel>
+          <Grid item xs={2} md={3} sx={{marginTop: "30px"}}>
+            <Box sx={{ }}>
+              <Box sx={{ height: "200px", width: "100%"}}>
+    
+                  <Carousel
+                    id="carouselStyle"
+                    indicators={true}
+                    autoPlay={false}
+                    sx={{  height: "200px", textAlign:"center" }}  /*change something with the height of carousel */
+                  >
+                    <div id="thumbnail">
+                      <img id="thumbnailImg" src={item}></img>
+                    </div>
+                  </Carousel>
               </Box>
-              <Box sx={{ border: "1px solid red" }}>
+              <Box sx={{}}>
                 <div id="itemDescription">
                   <Grid container spacing={3}>
                     <Grid item>Name</Grid>
@@ -68,6 +84,11 @@ export default function Matches() {
           </Grid>
         ))}
       </Grid>
+      </div>
+
+        {/* figure out how ot send these to the back  */}
+      <img id="blob1" src={blob1}></img>
+      <img id="blob2" src={blob2}></img>
     </>
   );
 }
