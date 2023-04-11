58d1c6cc12bfc7423d73eee98957986ce52fd100
Christian Bryan <cdbryan@usc.edu>
2023-04-12 00:15:20 +0200

    added logo and attribution to navbar

--- reco_eng_app/src/navbar/navbar.jsx
+++ reco_eng_app/src/navbar/navbar.jsx
@@ -4,9 +4,10 @@ import { useNavigate } from 'react-router-dom';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import "./navbar.css"
+import coffeeMug from "../images/coffeeMug.jpg"
 
 
-//! I want to change the background color of navbar buttons but mui buttons are being difficult 
+//Coffee mug image by <a href="https://www.freepik.com/free-vector/flat-design-cafe-signage_22203365.htm#query=solo%20steaming%20coffee%20mug&position=3&from_view=search&track=ais">Freepik</a>
 
 export default function Navbar(){
     const navigate = useNavigate();
@@ -31,7 +32,7 @@ export default function Navbar(){
     return(
         <>
         <div id="navbar">
-            <div id="logo"> <a href="/"> Logo </a> </div>
+            <div id="logo"> <a href="/"> <img src={coffeeMug} style={{maxHeight: "44px", marginRight: "20px"}}></img> </a> </div>
             <Stack spacing={10} direction="row" id="buttonStack" style={{justifyContent: "right", width: "82%", marginBottom: "1vh", marginTop:"1vh"}}>
                 <Button variant="text" style={{color : "black", fontSize: "17px", textTransform: 'none',  ":hover": {bgcolor: "#AF5",color: "white"}}} onClick={() => navigate("/howItWorks")} > How it Works? </Button>
                 <Button variant="text" style={{color : "black", fontSize: "17px", textTransform: 'none'}} onClick={() => navigate("/aboutUs")} > About Us </Button>
