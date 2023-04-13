c16c249bf2ace5286f675715bf7a3ca8b3fd4b72
Christian Bryan <cdbryan@usc.edu>
2023-04-13 16:50:16 +0200

    category ratings vector working finally :,)

--- reco_eng_app/src/backend/categoryVector.js
+++ reco_eng_app/src/backend/categoryVector.js
@@ -8,14 +8,30 @@ import {
 
 
 export const getCategoryRatingbyUser = () =>{
+  var vector = []; 
   const db = getDatabase();
-  const reference = ref(db, '/categoryRatings');
   
+  const reference = ref(db, '/categoryRatings');
   // Attach an asynchronous callback to read the data at our posts reference
   onValue(reference, (snapshot) => {
     console.log(snapshot.val());
+    snapshot.forEach(element => {
+      var rating = element.val().rating;
+      var categoryID = element.val().categoryID;
+      console.log("rating: " + rating);
+      vector.push(rating);    
+    });
+
+    vector.map((item, i) => {
+      console.log("item at index: " + i + " " + item)
+    })
+
   }, (errorObject) => {
     console.log('The read failed: ' + errorObject.name);
   });
+
+  return vector;
 }
+
+
 
